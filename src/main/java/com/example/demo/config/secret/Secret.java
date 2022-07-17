199b4b9eb3c2b658f30fcfbc886b61b4b213ebf2
parktj <ekswn120@gmail.com>
2022-07-17 21:02:15 +0900

    please work

--- src/main/java/com/example/demo/config/secret/Secret.java
+++ src/main/java/com/example/demo/config/secret/Secret.java
@@ -1,7 +1,9 @@
 package com.example.demo.config.secret;
 
-public static String JWT_SECRET_KEY = "abcde";
-public static String USER_INFO_PASSWORDKEY = "o9pqYVC9-F8.PEzEiw!L9F6.AYj9jcfVJ_i.ifXYnyE68kix@Q2dL6rwbV-rpdZYwcqZG-jPF-fw3CiJyKsfZ778ks-jnZn";
 // TODO: 해당 KEY 값들을 꼭 바꿔서 사용해주세요!
 // TODO: .gitignore에 추가하는거 앚지 마세요!
 
+public class Secret {
+    public static String JWT_SECRET_KEY = "abcde";
+    public static String USER_INFO_PASSWORD_KEY = "o9pqYVC9-F8.PEzEiw!L9F6.AYj9jcfVJ_i.ifXYnyE68kix@Q2dL6rwbV-rpdZYwcqZG-jPF-fw3CiJyKsfZ778ks-jnZn";
+}
